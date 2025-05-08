573ed84422e1af2fe744c02cf295a603622606f4
lawrenceagustin <167160419+lawrenceagustin@users.noreply.github.com>
2025-05-09 00:12:58 +0800

    Delete handle_ajax_request.php

--- handle_ajax_request.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['testAjaxRequest'])) {
-
-	echo "Hello there and welcome, " . $_POST['first_name'] 
-		 . " " . $_POST['last_name'] . 
-		 ". AJAX request sent to this file is successful 
-		 and yes, legit, this is from the PHP script!"; 
-}
-?>
